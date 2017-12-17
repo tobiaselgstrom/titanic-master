d7c4377d5e3a05a1c4dbed7151dcc8f840c04f19
tobiaselgstrom <32430088+tobiaselgstrom@users.noreply.github.com>
2017-12-17 15:05:38 +0100

    Modified main.py for Titanic Kaggle

--- main.py
+++ main.py
@@ -1,2 +1,9 @@
-print("Titanic Kaggle Competition")
+from sklearn.ensemble import RandomForestRegressor
+
+from sklearn.metrics import roc_auc_score
+
+import pandas as pd
+
+X = pd.read_csv("data/train.csv")
+y = X.pop("Survived")
 
