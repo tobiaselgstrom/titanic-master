144d10793237cae6de059d6e72c1ea1b3503cd28
tobiaselgstrom <32430088+tobiaselgstrom@users.noreply.github.com>
2017-12-17 15:55:52 +0100

    Model Trained

--- main.py
+++ main.py
@@ -7,3 +7,18 @@ import pandas as pd
 X = pd.read_csv("data/train.csv")
 y = X.pop("Survived")
 
+X["Age"].fillna(X.Age.mean(), inplace=True)
+# X.describe()
+
+numeric_variables = list(X.dtypes[X.dtypes != "object"].index)
+
+# print(numeric_variables)
+X[numeric_variables].head()
+
+model = RandomForestRegressor(n_estimators=100, oob_score=True, random_state=42)
+
+model.fit(X[numeric_variables], y)
+
+print(model.oob_score_)
+
+
